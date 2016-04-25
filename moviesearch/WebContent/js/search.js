372862d350c73fc405916a80895c565a0a9206bf
Niranth Reddy <niranthv@gmail.com>
2016-04-25 16:31:36 +0530

    Whole project

---
+++ moviesearch/WebContent/js/search.js
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+'use strict';
+
+angular.module('myApp', [])
+  .controller('MovieController', function($scope, $http) {
+    $scope.$watch('search', function() {
+      fetch();
+    });
+
+    $scope.search = "sherlock";
+
+    function fetch() {
+      $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+        .then(function(response) {
+          $scope.details = response.data;
+        });
+
+      $http.get("http://www.omdbapi.com/?s=" + $scope.search)
+        .then(function(response) {
+          $scope.related = response.data;
+        });
+    }
+
+    $scope.update = function(movie) {
+      $scope.search = movie.Title;
+    };
+
+    $scope.select = function() {
+      this.setSelectionRange(0, this.value.length);
+    }
+  });
