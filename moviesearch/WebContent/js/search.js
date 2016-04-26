ad3ff5c884c1171e07b59f5ecd8d7783b477a2d5
Niranth Reddy <niranthv@gmail.com>
2016-04-26 15:45:14 +0530

    Added Route Configuration to project

--- moviesearch/WebContent/js/search.js
+++ moviesearch/WebContent/js/search.js
@@ -4,8 +4,47 @@
 
 'use strict';
 
-angular.module('myApp', [])
-  .controller('MovieController', function($scope, $http) {
+
+var module = angular.module("myApp", ['ngRoute']);
+
+module.config(function($routeProvider) {
+	$routeProvider
+
+	// route for the home page
+	.when('/', {
+		templateUrl : 'pages/home.html',
+		controller  : 'mainController'
+	})
+
+	// route for the about page
+	.when('/moviesbyname', {
+		templateUrl : 'pages/moviesbyname.html',
+		controller  : 'MovieController'
+	})
+
+	// route for the contact page
+	.when('/contact', {
+		templateUrl : 'pages/contact.html',
+		controller  : 'contactController'
+	});
+});
+
+
+
+// create the controller and inject Angular's $scope
+module.controller('mainController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+//create the controller and inject Angular's $scope
+module.controller('contactController', function($scope) {
+	// create a message to display in our view
+	$scope.message = 'Everyone come and see how good I look!';
+});
+
+
+module.controller('MovieController', function($scope, $http) {
     $scope.$watch('search', function() {
       fetch();
     });
@@ -28,7 +67,7 @@ angular.module('myApp', [])
       $scope.search = movie.Title;
     };
 
-    $scope.select = function() {
-      this.setSelectionRange(0, this.value.length);
+    $scope.select = function(){
+    	this.setSelectionRange(0, this.value.length);
     }
   });
